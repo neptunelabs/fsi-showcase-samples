bb76d617cc5435e16e5cfa40fe3a7c8f90f453a8
biernat <Manimo12#git>
2021-08-13 15:48:07 +0200

    added sample

--- samples/pdp-2/pdp.js
+++ samples/pdp-2/pdp.js
@@ -1,30 +1,23 @@
-function switchImageSample() {
-  // add "click" handler to all 3 thumbnail images
-  for (var i = 1; i < 4; i++){
-    var el = document.getElementById("img" + i);
-    $FSI.addEvent(el, "click", onThumbnailClick);
-  }
-}
-
-
-function onThumbnailClick(){
-  var img = this.getElementsByTagName("img");
+function changeImage(buttonID){
   let imagesrc;
-  switch (img) {
-    case "img1":
+  let src;
+  switch (buttonID) {
+    case "0":
       imagesrc =  'images/samples/showcase/pdp/kate-skumen-3FoSvueTVCk-unsplash.jpg, images/samples/showcase/pdp/kate-skumen-h7FPeFBi3Mo-unsplash.jpg, images/samples/showcase/pdp/kate-skumen-S6xhmSe7RdA-unsplash.jpg, images/samples/showcase/pdp/kate-skumen-Z_wBSL_fPzw-unsplash.jpg';
+      src = 'images/samples/showcase/pdp/kate-skumen-3FoSvueTVCk-unsplash.jpg';
       break
-   case "img2":
-     imagesrc = 'images/samples/showcase/pdp/kate-skumen-PJRabkuH3_Q-unsplash.jpg, images/samples/showcase/pdp/kate-skumen-n268Z4TSd6M-unsplash.jpg';
-      break
-    case "img3":
-      imagesrc = 'images/samples/showcase/pdp/kate-skumen-XsFiUIamdTo-unsplash.jpg, images/samples/showcase/pdp/kate-skumen-KFz7sRs0ddI-unsplash.jpg';
+   case "1":
+     imagesrc = 'images/samples/showcase/pdp/kate-skumen-PJRabkuH3_Q-unsplash.jpg, images/samples/showcase/pdp/kate-skumen-n268Z4TSd6M-unsplash.jpg, images/samples/showcase/pdp/kate-skumen-glmDobIx4o0-unsplash.jpg, images/samples/showcase/pdp/kate-skumen-PJRabkuH3_Q-unsplash-x.jpg';
+     src = 'images/samples/showcase/pdp/kate-skumen-PJRabkuH3_Q-unsplash.jpg';
+     break
+    case "2":
+      imagesrc = 'images/samples/showcase/pdp/kate-skumen-XsFiUIamdTo-unsplash.jpg, images/samples/showcase/pdp/kate-skumen-KFz7sRs0ddI-unsplash.jpg, images/samples/showcase/pdp/kate-skumen-XsFiUIamdTo-unsplash-x.jpg, images/samples/showcase/pdp/kate-skumen-KFz7sRs0ddI-unsplash-x.jpg';
+      src = 'images/samples/showcase/pdp/kate-skumen-XsFiUIamdTo-unsplash.jpg';
       break
   }
-  var parameters = {"imagesources" : imagesrc};
-  console.log (imagesrc)
-  var thumbbar = document.getElementById("thumbbar");
-  thumbbar.changeConfig(parameters)
+  document.getElementsByTagName("fsi-thumbbar")[0].changeConfig(undefined, {imagesources: imagesrc, autoCrop: "cc"});
+  document.getElementsByTagName("fsi-viewer")[0].changeConfig(undefined, {imagesrc: src});
+
 }
 
 
