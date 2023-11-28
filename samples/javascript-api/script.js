4f98809040cc9afea352fbd4cd14409d199e35b8
kranzhoff <kranzhoff@neptunelabs.com>
2023-11-28 16:15:21 +0100

    - revised thumbbar sample with custom tags

--- samples/javascript-api/script.js
+++ samples/javascript-api/script.js
@@ -1,59 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   document.getElementById('thumbBtn').addEventListener('click', () => {
-    openPopUp()
 
-    const ele = document.createElement("div");
-    ele.style.width = '1100px'
-    ele.style.height = '600px'
-    ele.setAttribute("id", 'zoomViewer');
-    document.getElementById('showcase').appendChild(ele);
+    const createNode = function(nodeName, attributes){
+      const ret = document.createElement(nodeName);
+      for (const key in attributes) ret.setAttribute(key, attributes[key]);
+      return ret;
+    };
 
-    const instance = new $FSI.Viewer(ele,{
-      src: 'images/samples/ssi/furniture/living-room-7547558.jpg',
-      debug: true,
-      plugins: 'resize,fullScreen',
-      skin: 'white',
-      fullScreenElement:'showcase',
-    });
-    instance.start();
+    const container = document.getElementById('showcase');
+    while (container.firstChild) container.removeChild(container.firstChild); // remove existing content
 
-    const thumbViewer = document.createElement("div");
-    thumbViewer.style.width = '1100px'
-    thumbViewer.style.height = '200px'
-    thumbViewer.setAttribute("id", 'thumbViewer');
-    document.getElementById('showcase').appendChild(thumbViewer);
+    container.appendChild(
+      createNode("fsi-viewer", {
+        id: "zoomViewer",
+        width: 1100,
+        height: 600,
+        skin: 'white',
+        debug: true
+      })
+    )
 
-    let thumbInstance = new $FSI.ThumbBar(thumbViewer, {
-      width:'100%',
-      height:'200px',
-      debug:'true',
-      imagesources:'images/samples/ssi/furniture/living-room-7547558.jpg, images/samples/ssi/furniture/living-room-7547559.jpg,\timages/samples/ssi/furniture/home-7547557.jpg',
-      useTouchZoom:'false',
-      elementWidth:'350px',
-      elementSpacing:'4px',
-      alignment:'0.5',
-      paddingTop:'0',
-      autoCrop:'cc',
-      onInit: () => {
-        // assignViewer
-        addViewer()
-      },
-    }, true)
-
-    function addViewer () {
-      // toggle page index
-      thumbInstance.assignFSIViewer('zoomViewer')
-    }
+    container.appendChild(
+      createNode("fsi-thumbbar", {
+        src: "images/samples/ssi/furniture/living-room-7547558.jpg",
+        width: "100%",
+        height: 200,
+        debug: true,
+        imagesources: "images/samples/ssi/furniture/living-room-7547558.jpg, images/samples/ssi/furniture/living-room-7547559.jpg,\timages/samples/ssi/furniture/home-7547557.jpg",
+        useTouchZoom: false,
+        elementWidth: "350px",
+        elementSpacing: "4px",
+        alignment: 0.5,
+        paddingTop: 0,
+        autoCrop: "cc",
+        viewerSelector: "#zoomViewer"
+      })
+     );
 
   })
-
-
-  function openPopUp(evt, oCell) {
-    const modalToggle = document.getElementById('myModal')
-    modalToggle.addEventListener('hidden.bs.modal', (event) => {
-      document.getElementById('thumbViewer').assignFSIViewer('zoomViewer')
-    })
-  }
-
 })
