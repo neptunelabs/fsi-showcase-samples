ac9dd18f9864b5fb9578de0c2466b1f9aa0bcb18
nelabiernat <60930527+nelabiernat@users.noreply.github.com>
2024-02-29 15:21:25 +0100

    react update

--- samples/react/sample.js
+++ samples/react/sample.js
@@ -1,6 +1,9 @@
 const Sample = () => {
   return (
-    <div className='fsi-viewer-section'>
+    <div className="container mx-auto">
+      <div className="row row-cols-1 g-3 px-5 py-3">
+        <div className="col-lg-8">
+          <div className='fsi-viewer-section'>
       <div
         className='showcase'
         id={"fsi-showcase-1689840468239-713615"}>
@@ -23,13 +26,38 @@ const Sample = () => {
           height={"160px"}
           dir={"images/samples/showcase/tutorial"}
           vertical={false}
-          elementWidth={"240px"}
+          elementWidth={"150px"}
           elementSpacing={"4px"}
           alignment={0.5}
           paddingTop={0}
           paddingBottom={"4px"}
           debug={true}
         />
+      </div>
+    </div>
+        </div>
+        <div className="col-lg-4">
+          <div className="p-3 text-left">
+            <h2 className="display-5">Camu</h2>
+            <p>Sneaker, Camouflage Suede</p>
+            <p>
+              <i className="bi bi-star-fill"></i>
+              <i className="bi bi-star-fill"></i>
+              <i className="bi bi-star-fill"></i>
+              <i className="bi bi-star-fill"></i>
+              <i className="bi bi-star-half"></i>
+              <small>(4664) Ratings</small>
+            </p>
+            <p>Sturdy, classy and minimal.</p>
+            <p>Nice shoe for any occasion. The minimal design fits a lot of outfits.</p>
+            <p>
+              <small>Dispatched in 4 working days</small>
+            </p>
+            <div className="btn-group py-3">
+              <button type="button" className="btn btn-lg btn-outline-dark">Add to basket</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
